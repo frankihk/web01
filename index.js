aaf495f0c3049ebd1ad11ef56ca41f48aa57b456
Franki Ho <franki.hk@gmail.com>
2024-05-10 07:49:22 +0000

    test

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const dates = [
   new Date(1995, 6, 2),
   new Date(1987, 1, 11),
   new Date(1989, 6, 10),
+  
 ];
 console.log(dates.sort(compareAsc));
+
 //=> [
 //   Wed Feb 11 1987 00:00:00,
 //   Mon Jul 10 1989 00:00:00,
